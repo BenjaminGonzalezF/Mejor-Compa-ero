597b0c77d66c98ed6de86c2674ee603dd76f20e0
BenjaminGonzalezF <begonzalez18@alumnos.utalca.cl>
2023-10-18 10:55:47 -0300

    Integración del componente Login con la base de datos
    -Se conecta lo obtenido desde los elementos input con los datos en la base de datos

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './components/Login.jsx' 
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+{
+  path: '/', 
+  element: <Login/>,
+},
+{
+  path: '/inicio', 
+  element: <App/>,
+}
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
